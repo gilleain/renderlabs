2b0185dd520dd2585d28102bd4890382a468b8e0
gilleain torrance <gilleain.torrance@gmail.com>
2008-11-23 14:08:21 +0000

    fixed tabs

--- experiments/chalky/src/chalky/Main.java
+++ experiments/chalky/src/chalky/Main.java
@@ -1,20 +1,23 @@
 package chalky;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Main extends JPanel {
-	
-	public Main() {
-		
-	}
-	
-	public static void main(String[] args) {
-		JFrame f = new JFrame();
-		Main m = new Main();
-		f.add(m);
-		f.pack();
-		f.setVisible(true);
-	}
+
+    public Main() {
+        this.setSize(400, 400);
+        this.setBackground(Color.BLACK);
+    }
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        Main m = new Main();
+        f.add(m);
+        f.pack();
+        f.setVisible(true);
+    }
 
 }
